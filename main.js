c17c8c8e453733282f75b33fe1108ff6421dfea7
thientn20 <thientn20@uef.edu.vn>
2024-05-18 11:50:04 +0700

    update code

--- main.js
+++ main.js
@@ -3,62 +3,65 @@ const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
+	navLinks.classList.toggle("open");
 
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+	const isOpen = navLinks.classList.contains("open");
+	menuBtnIcon.setAttribute(
+		"class",
+		isOpen ? "ri-close-line" : "ri-menu-line"
+	);
 });
 
 navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
+	navLinks.classList.remove("open");
+	menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
 const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
+	distance: "50px",
+	origin: "bottom",
+	duration: 1000,
 };
 
 ScrollReveal().reveal(".header__image img", {
-  ...scrollRevealOption,
-  origin: "right",
+	...scrollRevealOption,
+	origin: "right",
 });
 ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 500,
+	...scrollRevealOption,
+	delay: 500,
 });
 ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 1000,
+	...scrollRevealOption,
+	delay: 1000,
 });
 ScrollReveal().reveal(".header__image__content ", {
-  duration: 1000,
-  delay: 1500,
+	duration: 1000,
+	delay: 1500,
 });
 
 ScrollReveal().reveal(".product__image img", {
-  ...scrollRevealOption,
-  origin: "left",
+	...scrollRevealOption,
+	origin: "left",
 });
 ScrollReveal().reveal(".product__card", {
-  ...scrollRevealOption,
-  delay: 500,
-  interval: 500,
+	...scrollRevealOption,
+	delay: 500,
+	interval: 500,
 });
 ScrollReveal().reveal(".product__btn", {
-  ...scrollRevealOption,
-  delay: 500,
+	...scrollRevealOption,
+	delay: 500,
 });
 
 ScrollReveal().reveal(".store__container h2, .store__container p", {
-  ...scrollRevealOption,
-  delay: 500,
+	...scrollRevealOption,
+	delay: 500,
 });
 
 ScrollReveal().reveal(".store__container .swiper", {
-  ...scrollRevealOption,
-  delay: 1000,
+	...scrollRevealOption,
+	delay: 1000,
 });
 
 ScrollReveal().reveal(".contact__container .section__header", {
@@ -75,21 +78,21 @@ ScrollReveal().reveal(".contact__input", {
 });
 
 const swiper = new Swiper(".swiper", {
-  loop: true,
-  effect: "coverflow",
-  grabCursor: true,
-  centerSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    depth: 250,
-    modifier: 1,
-    scale: 0.75,
-    slideShadows: false,
-    stretch: -100,
-  },
+	loop: true,
+	effect: "coverflow",
+	grabCursor: true,
+	centerSlides: true,
+	slidesPerView: "auto",
+	coverflowEffect: {
+		rotate: 0,
+		depth: 250,
+		modifier: 1,
+		scale: 0.75,
+		slideShadows: false,
+		stretch: -100,
+	},
 
-  pagination: {
-    el: ".swiper-pagination",
-  },
+	pagination: {
+		el: ".swiper-pagination",
+	},
 });
